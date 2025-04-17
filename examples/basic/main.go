d1eef66b4255b0dbc8d99a6deb3a36ec1e7c80cb
CK Ng <ngchoonkhon@gmail.com>
2025-04-17 10:51:20 +0800

    feat: add int64 method to return int64 ID

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -24,16 +24,18 @@ func main() {
 	}
 
 	// ID 1
-	fmt.Printf("ID: %s\n", id1.String())                // output, eg. 37866498659848192
-	fmt.Printf("ID (Binary): %s\n", id1.StringBinary()) // output, eg. 0000000010000110100001110110000101000001100000100001000000000000
+	fmt.Printf("ID (string): %s\n", id1.String())       // output, eg. "37866498659848192"
+	fmt.Printf("ID (Binary): %s\n", id1.StringBinary()) // output, eg. "0000000010000110100001110110000101000001100000100001000000000000"
+	fmt.Printf("ID (Decimal): %d\n", id1.Int64())       // output, eg. 37866498659848192
 	fmt.Printf("Sequence: %d\n", id1.SequenceNumber)    // sequence 0
 	// ID 2
-	fmt.Printf("ID: %s\n", id2.String())                // outpuot, eg. 37866498659848193
-	fmt.Printf("ID (Binary): %s\n", id2.StringBinary()) // output, eg. 0000000010000110100001110110000101000001100000100001000000000001
+	fmt.Printf("ID: %s\n", id2.String())                // outpuot, eg. "37866498659848193"
+	fmt.Printf("ID (Binary): %s\n", id2.StringBinary()) // output, eg. "0000000010000110100001110110000101000001100000100001000000000001"
+	fmt.Printf("ID (Decimal): %d\n", id2.Int64())       // output, eg. 37866498659848192
 	fmt.Printf("Sequence: %d\n", id2.SequenceNumber)    // sequence 1
 
 	// Parse ID
 	id1Copy := id1.String()
 	reverseParseId1, _ := snowid.ParseId(id1Copy, snowid.DefaultEpoch)
-	fmt.Printf("ID: %s\n", reverseParseId1.String()) // same id as ID 1 after parsing, ie. 37866498659848192
+	fmt.Printf("ID: %s\n", reverseParseId1.String()) // same id as ID 1 after parsing, ie. "37866498659848192"
 }
